8eebc2dc7bcb3cc418ba476b4091c4a3c0199225
Bruce <irabruce10@gmail.com>
2024-10-24 23:23:34 +0200

    Adding profile link

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,12 +9,14 @@ export default function Home() {
       <FlatList
         data={[{ id: '1' }, { id: '2' }, { id: '3' }]}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => <Text>Item {item.id}</Text>}
+        renderItem={({ item }) => (
+          <Text className="text-white">Item {item.id}</Text>
+        )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
               <View>
-                <Text className="font-pmedium text-sm text-gray-100 ">
+                <Text className="font-psemibold  text-2xl  text-gray-100 ">
                   Welcome
                 </Text>
               </View>
