43a95f5622772fba88c3f1e1fc07eceb2537aa35
Bruce <irabruce10@gmail.com>
2024-10-27 22:04:02 +0100

    Connecting Database Postgresql

--- migrations/00000-createTableProducts.ts
+++ migrations/00000-createTableProducts.ts
@@ -1,7 +1,42 @@
-export async function up(client) {
+import type { Sql } from 'postgres';
+import { z } from 'zod';
 
+export type Product = {
+  id: number;
+  name: string;
+  price: string;
+  description: string;
+  category: string | null;
+  image: string;
+  quantity: number;
+  countInStock: number | null;
+};
+
+export const productsSchema = z.object({
+  name: z.string().min(1).max(50),
+  description: z.string().min(1).max(250),
+  image: z.string().min(1).max(250),
+  category: z.string().nullable(),
+  price: z.number().min(0),
+  quantity: z.number().min(1),
+  countInStock: z.number().nullable(),
+});
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE products (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      name varchar(50) NOT NULL,
+      price numeric CHECK (price > 0) NOT NULL,
+      description varchar(250) NOT NULL,
+      category varchar(250),
+      image varchar(250) NOT NULL,
+      quantity integer NOT NULL,
+      count_in_stock integer
+    );
+  `;
 }
 
-export async function down(client) {
-
+export async function down(sql: Sql) {
+  await sql` DROP TABLE products; `;
 }
