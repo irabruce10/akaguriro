978cc00896dcc1c743378f3e39e81b39cb6c1cc7
Bruce <irabruce10@gmail.com>
2024-10-27 20:01:14 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Akaguriro!</Text>
+      <Text>Akaguriro store!</Text>
       <StatusBar style="auto" />
     </View>
   );
