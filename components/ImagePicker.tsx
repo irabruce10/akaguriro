cf1cbf1d749899c9de9893ca5c42ee066cfdc06d
Bruce <irabruce10@gmail.com>
2024-10-31 23:19:53 +0100

    Fixing some bug

--- components/ImagePicker.tsx
+++ components/ImagePicker.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { Button, Image, View, StyleSheet } from 'react-native';
+import { Button, Image, View, StyleSheet, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function ImagePickerExample() {
-  const [image, setImage] = useState<string | undefined>(undefined);
+  // const [image, setImage] = useState<string | undefined>(undefined);
+  const [image, setImage] = useState<string[]>([]);
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -12,20 +13,32 @@ export default function ImagePickerExample() {
       allowsMultipleSelection: true,
       aspect: [4, 3],
       quality: 1,
+      orderedSelection: true,
     });
 
     console.log(result);
 
     if (!result.canceled) {
-      setImage(result.assets[0]?.uri);
+      // setImage(result.assets[0]?.uri);
+      const imageUris = result.assets.map((asset) => asset.uri);
+      setImage(imageUris);
+    } else {
+      alert('No image selected');
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={styles.image} />}
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {/* {image && <Image source={{ uri: image }} style={styles.image} />} */}
+
+        {image.map((image, index) => (
+          <Image key={index} source={{ uri: image }} style={styles.image} />
+        ))}
+
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+      </View>
+    </ScrollView>
   );
 }
 
