c490a6a27bd9e691f353c6a8e13131c1668a4a0c
Bruce <irabruce10@gmail.com>
2024-11-13 13:21:28 +0100

    Setup chat

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ScrollView, StatusBar, Text, View } from 'react-native';
 import React from 'react';
 import { Link, router } from 'expo-router';
@@ -6,24 +7,26 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaView className="w-full flex justify-center items-center px-4 min-h-[105vh]">
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className=" w-full justify-center items-center h-full px-4">
-          <Text className="text-sm font-pblack   text-center">Akaguriro</Text>
+    <GestureHandlerRootView>
+      <SafeAreaView className="w-full flex justify-center items-center px-4 min-h-[105vh]">
+        <ScrollView contentContainerStyle={{ height: '100%' }}>
+          <View className=" w-full justify-center items-center h-full px-4">
+            <Text className="text-sm font-pblack   text-center">Akaguriro</Text>
 
-          <Text>
-            <Link href="/home" style={{ color: 'blue' }}>
-              go to home page
-            </Link>
-          </Text>
-          <CustomButton
-            title="Continue with Login in"
-            handlePress={() => router.push('/authModal/signin')}
-            containerStyles="w-full flex justify-center items-center mt-4 px-4 py-2 rounded-md text-sm font-pblack text-white bg-primary"
-            textStyles=""
-          />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+            <Text>
+              <Link href="/home" style={{ color: 'blue' }}>
+                go to home page
+              </Link>
+            </Text>
+            <CustomButton
+              title="Continue with Login in"
+              handlePress={() => router.push('/authModal/signin')}
+              containerStyles="w-full flex justify-center items-center mt-4 px-4 py-2 rounded-md text-sm font-pblack text-white bg-primary"
+              textStyles=""
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
