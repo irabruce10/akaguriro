02437886b959afb495e06b3102c54c317d114893
Bruce <irabruce10@gmail.com>
2024-11-22 15:33:55 +0100

    Add Carousel

--- app/(app)/(tabs)/home.tsx
+++ app/(app)/(tabs)/home.tsx
@@ -1,14 +1,86 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React, { useCallback } from 'react';
-import { StreamChat } from 'stream-chat';
 
-const client = StreamChat.getInstance('fvp2pqwqbcsh');
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Animated, {
+  FadeInLeft,
+  FadeInRight,
+  FadeInUp,
+} from 'react-native-reanimated';
+import Carousel from '../../../components/Carousel';
+
 export default function Home() {
   return (
-    <View>
-      <Text>Home ...a </Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <StatusBar barStyle="light-content" backgroundColor="#161622" />
+      <ScrollView className="">
+        <View className="flex flex-row px-4 justify-between mt-7">
+          <Animated.Text
+            entering={FadeInUp.delay(300).duration(300)}
+            className="text-base text-gray-100 font-pmedium "
+          >
+            Welcome Again
+          </Animated.Text>
+          <Animated.Image
+            entering={FadeInUp.delay(500).duration(500)}
+            source={require('../../../assets/AkaLogo.png')}
+            resizeMode="cover"
+            style={styles.logo}
+          />
+        </View>
+        <View>
+          <Carousel />
+        </View>
+
+        <View className="px-4">
+          <Animated.Text
+            entering={FadeInRight.delay(600).duration(600)}
+            className="text-xl text-center mt-6 mb-6 text-gray-100 font-pmedium "
+          >
+            Why Choose Us?
+          </Animated.Text>
+          <View>
+            <Animated.Text
+              entering={FadeInLeft.delay(700).duration(700)}
+              className="text-xl text-gray-100 font-pmedium "
+            >
+              Choose from a variety of apartments that suit your needs, whether
+              you're traveling solo, with family, or for business.
+            </Animated.Text>
+          </View>
+
+          <View>
+            <Animated.Text
+              entering={FadeInLeft.delay(700).duration(700)}
+              className="text-xl mt-6 text-gray-100 font-pmedium "
+            >
+              Book your apartment in just a few steps—no hidden fees.
+            </Animated.Text>
+          </View>
+          <View>
+            <Animated.Text
+              entering={FadeInLeft.delay(700).duration(700)}
+              className="text-xl mt-6 text-gray-100 font-pmedium "
+            >
+              Great Locations: Our apartments are located in prime areas.
+            </Animated.Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  logo: {
+    width: 50,
+    height: 50,
+    top: -20,
+  },
+});
---
+++ components/Carousel.tsx
@@ -0,0 +1,123 @@
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+} from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { FadeInLeft } from 'react-native-reanimated';
+
+export default function Carousel() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const screenWidth = Dimensions.get('window').width;
+  const flatListRef = useRef();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (activeIndex === images.length - 1) {
+        setActiveIndex(0);
+        flatListRef?.current?.scrollToIndex({
+          index: 0,
+          animated: true,
+          FadeInLeft,
+        });
+      } else {
+        flatListRef?.current?.scrollToIndex({
+          index: activeIndex + 1,
+          animated: true,
+        });
+      }
+      setActiveIndex(activeIndex);
+    }, 2000);
+    return () => clearInterval(interval);
+  });
+  const images = [
+    {
+      id: 1,
+      image: require('../assets/images/pc.jpg'),
+    },
+    {
+      id: 2,
+
+      image: require('../assets/images/pc1.jpg'),
+    },
+    {
+      id: 3,
+      image: require('../assets/images/3.jpg'),
+    },
+  ];
+
+  const getItemLayout = (data: any, index: number) => {
+    return {
+      length: screenWidth,
+      offset: screenWidth * index,
+      index,
+    };
+  };
+
+  const handleScroll = (event: any) => {
+    //get the scroll position
+    const scrollPosition = event.nativeEvent.contentOffset.x;
+
+    //get the index of current active
+
+    const activeIndex = Math.round(scrollPosition / screenWidth);
+
+    setActiveIndex(activeIndex);
+  };
+
+  const renderDotIndicator = () => {
+    return images.map((dot, index) => {
+      //set the active dot to white color
+      const activeDotColor = index === activeIndex ? '#ea831c' : '#ccc';
+
+      //return the dot
+      return (
+        <View
+          key={index}
+          style={{
+            width: 8,
+            height: 8,
+            borderRadius: 4,
+            marginHorizontal: 6,
+            backgroundColor: activeDotColor,
+          }}
+        />
+      );
+    });
+  };
+
+  const renderItems = ({ item, index }: { item: any; index: number }) => {
+    return (
+      <View>
+        <Image
+          source={item.image}
+          style={{ height: 200, width: screenWidth }}
+        />
+      </View>
+    );
+  };
+  return (
+    <View>
+      <Text className="text-center mb-6 text-xl text-white ">
+        Modern Luxury Apartments
+      </Text>
+      <FlatList
+        data={images}
+        ref={flatListRef}
+        getItemLayout={getItemLayout}
+        renderItem={renderItems}
+        keyExtractor={(item: any) => String(item.id)}
+        horizontal={true}
+        pagingEnabled={true}
+        onScroll={handleScroll}
+      />
+
+      <View className="flex flex-row  justify-center mt-6 ">
+        {renderDotIndicator()}
+      </View>
+    </View>
+  );
+}
