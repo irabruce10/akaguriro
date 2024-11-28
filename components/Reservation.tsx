13d73fd497eb9c7d0e87223f76dc53f011e5d4cf
Bruce <irabruce10@gmail.com>
2024-11-28 17:25:52 +0100

    Create Readme file

--- components/Reservation.tsx
+++ components/Reservation.tsx
@@ -23,27 +23,49 @@ const Reservation = ({ booking }: Props) => {
   return (
     <View className="my-6 px-4 space-y-6">
       <>
-        <View>
-          <Text className="text-white">Dash Apartment</Text>
-          <Text className="text-white">StartDate :{startDate}</Text>
-          <Text className="text-white">EndDate: {endDate}</Text>
-          <Text className="text-white">Guests: {numGuests}</Text>
-          <Text className="text-white">Nights: {numNights}</Text>
-        </View>
-
-        <View>
-          <Pressable
-            onPress={async () => {
-              await fetch(`/api/reservationDash/${id}`, {
-                method: 'DELETE',
-              });
-              router.replace('/reservationDash/reservationDash');
-            }}
+        <View className="flex flex-row ">
+          <View>
+            <Text className="text-gray-100 px-4  text-lg font-pregular ">
+              CheckIn :{startDate}
+            </Text>
+            <Text className="text-gray-100 px-4  text-lg font-pregular ">
+              CheckOut: {endDate}
+            </Text>
+            <Text className="text-gray-100 px-4  text-lg font-pregular ">
+              Guests: {numGuests}
+            </Text>
+            <Text className="text-gray-100 px-4 mb-6  text-lg font-pregular ">
+              Nights: {numNights} nights
+            </Text>
+          </View>
+          <View
+            className="bg-secondary  px-4 rounded-xl min-h-[62px] justify-center mt-16  text-xl font-medium  text-center "
+            style={{ top: -42, left: 92 }}
           >
-            <Ionicons name="trash-outline" size={36} />
-          </Pressable>
+            <Pressable
+              onPress={async () => {
+                await fetch(`/api/reservationDash/${id}`, {
+                  method: 'DELETE',
+                });
+                router.replace('/reservationDash/reservationDash');
+              }}
+            >
+              <Ionicons name="trash-outline" size={36} />
+            </Pressable>
+          </View>
         </View>
       </>
+      <View className="flex-shrink-0 mb-6 flex flex-row ">
+        <View className="flex flex-row  mb-14  text-center items-center ml-10   ">
+          <Link
+            href="/(tabs)/apartments"
+            style={{ color: 'black' }}
+            className="bg-secondary px-9 rounded-xl min-h-[62px] justify-center mt-5 pt-5 text-xl font-medium  text-center "
+          >
+            <Text>Go Back</Text>
+          </Link>
+        </View>
+      </View>
     </View>
   );
 };
