7884dcfb395177fdeaf2d32876d96a454bba8379
Bruce <irabruce10@gmail.com>
2024-12-05 13:19:34 +0100

    Fixing the bug

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,56 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+
+// import { getStorage } from 'firebase/storage';
+// import { getFirestore } from 'firebase/firestore';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAkQT4UQbJ5UnZgQSGKE5aJMOKofqjCqgQ',
+//   authDomain: 'apartmentstorage-20324.firebaseapp.com',
+//   projectId: 'apartmentstorage-20324',
+//   storageBucket: 'apartmentstorage-20324.firebasestorage.app',
+//   messagingSenderId: '973891081804',
+//   appId: '1:973891081804:web:0ad4021305a51576a8c6e9',
+//   measurementId: 'G-4ML1R9KRTS',
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// import { getStorage } from 'firebase/storage';
+// import { getFirestore } from 'firebase/firestore';
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAkQT4UQbJ5UnZgQSGKE5aJMOKofqjCqgQ',
+//   authDomain: 'apartmentstorage-20324.firebaseapp.com',
+//   projectId: 'apartmentstorage-20324',
+//   storageBucket: 'apartmentstorage-20324.firebasestorage.app',
+//   messagingSenderId: '973891081804',
+//   appId: '1:973891081804:web:2366d1075b826dcaa8c6e9',
+//   measurementId: 'G-FD6MJ66H24',
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// export const storage = getStorage(app);
+// export const db = getFirestore(app);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-
+import { getAnalytics } from 'firebase/analytics';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,16 +59,17 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAkQT4UQbJ5UnZgQSGKE5aJMOKofqjCqgQ',
-  authDomain: 'apartmentstorage-20324.firebaseapp.com',
-  projectId: 'apartmentstorage-20324',
-  storageBucket: 'apartmentstorage-20324.firebasestorage.app',
-  messagingSenderId: '973891081804',
-  appId: '1:973891081804:web:0ad4021305a51576a8c6e9',
-  measurementId: 'G-4ML1R9KRTS',
+  apiKey: 'AIzaSyBx_ztcjaYwPJd8csIYM0egZoX-Q5IIK-A',
+  authDomain: 'apartmstorage.firebaseapp.com',
+  projectId: 'apartmstorage',
+  storageBucket: 'apartmstorage.firebasestorage.app',
+  messagingSenderId: '957220082421',
+  appId: '1:957220082421:web:1402feebe5d8e283e6ebbc',
+  measurementId: 'G-3J07JN9Q25',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
