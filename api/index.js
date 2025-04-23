33a91c01448115355051c179b798c22b27cfcfb4
Bruce <irabruce10@gmail.com>
2025-04-23 19:03:22 +0200

    fix: use expo web build on Vercel

--- api/index.js
+++ api/index.js
@@ -1,18 +1,3 @@
-// import path from 'node:path';
-// import { createRequestHandler } from '@expo/server/adapter/vercel';
-
-// export default createRequestHandler({
-//   build: path.join(__dirname, '../dist/server'),
-
-// });
-
-
-// import path from 'node:path';
-// import { createRequestHandler } from '@expo/server/adapter/vercel';
-
-// export default createRequestHandler({
-//   build: path.join(__dirname, '../dist/server'),
-// });
 
 
 const path = require('node:path');
