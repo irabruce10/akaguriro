304529cfed88d56bf960239acd65395dbb4644be
Bruce <irabruce10@gmail.com>
2025-04-23 18:24:06 +0200

    fix: use expo web build on Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,12 @@
+// import path from 'node:path';
+// import { createRequestHandler } from '@expo/server/adapter/vercel';
+
+// export default createRequestHandler({
+//   build: path.join(__dirname, '../dist/server'),
+
+// });
+
+
 import path from 'node:path';
 import { createRequestHandler } from '@expo/server/adapter/vercel';
 
