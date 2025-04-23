547ea2b5754acaf112b6d0c15c6a5d9baf0ccef7
Bruce <irabruce10@gmail.com>
2025-04-23 18:12:15 +0200

    fix: use expo web build on Vercel

--- util/config.js
+++ util/config.js
@@ -1,34 +1,7 @@
-// import { config } from 'dotenv-safe';
-// import postgres from 'postgres';
+import { config } from 'dotenv-safe';
+import postgres from 'postgres';
 
-// export const postgresConfig = {
-//   ssl: Boolean(process.env.POSTGRES_URL),
-//   transform: {
-//     ...postgres.camel,
-//     undefined: null,
-//   },
-// };
-
-// export function setEnvironmentVariables() {
-//   if (process.env.NODE_ENV === 'production' || process.env.CI) {
-//     // Set standard environment variables for Postgres.js from
-//     // Vercel environment variables
-//     if (process.env.POSTGRES_URL) {
-//       process.env.PGHOST = process.env.POSTGRES_HOST;
-//       process.env.PGDATABASE = process.env.POSTGRES_DATABASE;
-//       process.env.PGUSERNAME = process.env.POSTGRES_USER;
-//       process.env.PGPASSWORD = process.env.POSTGRES_PASSWORD;
-//     }
-//     return;
-//   }
-//   config();
-// }
-
-
-const { config } = require('dotenv-safe');
-const postgres = require('postgres');
-
-const postgresConfig = {
+export const postgresConfig = {
   ssl: Boolean(process.env.POSTGRES_URL),
   transform: {
     ...postgres.camel,
@@ -36,8 +9,10 @@ const postgresConfig = {
   },
 };
 
-function setEnvironmentVariables() {
+export function setEnvironmentVariables() {
   if (process.env.NODE_ENV === 'production' || process.env.CI) {
+    // Set standard environment variables for Postgres.js from
+    // Vercel environment variables
     if (process.env.POSTGRES_URL) {
       process.env.PGHOST = process.env.POSTGRES_HOST;
       process.env.PGDATABASE = process.env.POSTGRES_DATABASE;
@@ -48,8 +23,3 @@ function setEnvironmentVariables() {
   }
   config();
 }
-
-module.exports = {
-  postgresConfig,
-  setEnvironmentVariables,
-};
