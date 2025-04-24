85df95ce806a3fd4f9397210f54097b93feac20e
Bruce <irabruce10@gmail.com>
2025-04-24 14:32:07 +0200

    fix:some bug

--- api/index.js
+++ api/index.js
@@ -1,11 +1,6 @@
-
-
 const path = require('node:path');
 const { createRequestHandler } = require('@expo/server/adapter/vercel');
 
 module.exports = createRequestHandler({
   build: path.join(__dirname, '../dist/server'),
 });
-// module.exports = createRequestHandler({
-//   build: path.join(__dirname, '../dist/client'),
-// });
