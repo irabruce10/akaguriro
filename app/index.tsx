62a555d83e0981c2e14503cd0786d5bf616cf6db
Bruce <irabruce10@gmail.com>
2025-04-24 15:02:47 +0200

    fix:some bug

--- app/index.tsx
+++ app/index.tsx
@@ -55,7 +55,7 @@ export default function App() {
                 <Text
                   style={{ left: -30, textAlign: 'center', fontWeight: '600' }}
                 >
-                  Continue with Email
+                  Continue with Email.
                 </Text>
               </View>
             </TouchableOpacity>
